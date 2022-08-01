4cd023860cf149eb7100cb6aa7652d2ffa4a59f7
VenkaSri <venkasritharan@gmail.com>
2022-08-01 17:36:03 -0400

    added gameboard

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const container = document.querySelector('#main-container');
+const playButton = document.querySelector('#play-button');
+
+const gameboard = (() => {
+
+    const boardarray = [];
+    
+    for(let i = 0; i < 9; i++) {
+        let div = document.createElement('div');
+        boardarray.push(div);
+    }
+    return {boardarray};
+    
+})();
+
+
+playButton.addEventListener("click", () => {
+for(let key in gameboard.boardarray) {
+    container.append(gameboard.boardarray[key])
+}
+})
+
+const borders = (() => {
+
+    for(let i = 0; i < 3; i++)  {
+        gameboard.boardarray[i].style.borderTop = 'none';
+        gameboard.boardarray[0].style.borderLeft = 'none';
+        gameboard.boardarray[2].style.borderRight = 'none';
+    }
+    for(let i = 3; i < 6; i++)  {
+        gameboard.boardarray[3].style.borderLeft = 'none';
+        gameboard.boardarray[5].style.borderRight = 'none';
+    }
+    for(let i = 6; i < 9; i++)  {
+        gameboard.boardarray[i].style.borderBottom = 'none';
+        gameboard.boardarray[6].style.borderLeft = 'none';
+        gameboard.boardarray[8].style.borderRight = 'none';
+    }
+  
+})();
+
+
+
+
+
+
+
+
