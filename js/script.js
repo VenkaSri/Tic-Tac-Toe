fa3f90b87604c1fa01a536797a00e197ef6a5fe6
VenkaSri <venkasritharan@gmail.com>
2022-08-31 18:44:21 -0400

    AI returna random spot

--- js/script.js
+++ js/script.js
@@ -1,83 +1,102 @@
 let xPlayer = true;
-let pos = 0;
-let gameOver = true;
+let gameOver = false;
+const emptySpotsArray = [];
+
 const gameboard = (() => {
+
   const  board = [[],[],[]];
+  const cell = document.querySelectorAll('.cell');
+  for (let i = 0; i < cell.length; i++) {
+    if(i < 3) board[0].push(cell[i]);
+    if(i < 6 && i >= 3) board[1].push(cell[i]);
+    if(i < 9 && i >= 6) board[2].push(cell[i]);
+  }
   return {board};
 })();
 
-const player = (name, mark) => {
-  const getName = () => name;
-  const getMark = () => mark;
-  return {getName, getMark}
+const player = (name, type) => {
+  const getName = name;
+  const getType = type;
+  return {getName, getType}
 }
 
 
-const player1 = player('Jim', 'x');
-const player2 = player('Pam', 'o');
+const player1 = player('Jim', 'AI');
+const player2 = player('AI', 'Human');
 
-const placeMark = (mark) => {
-    for (let i = 0; i < arr.length; i++) {
-      arr[i].addEventListener('click', () => {
-        if(arr[i].innerHTML !== mark) {
-          arr[i].innerHTML = mark;
-          turnArray.push(mark);
-          gameFlow();
-        }  
-      })
-    }
-  };
 
 window.onload = () => {
-  const cells = document.querySelectorAll('.cell');
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', () => {
-      pos = i;
-      if (gameOver) {
-        gameFlow();
+  const board = gameboard.board;
+  for (let i = 0; i < board.length; i++) {
+    for (let j= 0; j < board[i].length; j++) {
+      board[i][j].addEventListener('click', () => {
+        if(xPlayer)  { 
+          if(player1.getType == 'AI') {
+            board[i][j].innerHTML = "X";
+          } else {
+          }
+          xPlayer = false;
+        } else {
+          board[i][j].innerHTML = "O";
+          xPlayer = true;
+        }
+      }, {once:true})
+    }
+  }
+}
+
+const emptySpots = () => {
+  const board = gameboard.board;
+  let iPos = 0;
+  let jPos = 0;
+  emptySpotsArray.length = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if(board[i][j].innerHTML == "") {
+        iPos = i;
+        jPos = j;
+        
+        emptySpotsArray.push({iPos, jPos})
       }
-      
-    }, {once: true});
+    }
   }
+  return emptySpotsArray;
 }
 
-const getPosition = (pos) => {
-  if (pos >= 0 && pos < 3) {
-    let iPos = 0;
-    let jPos = pos;
-    return {iPos, jPos};
-  } else if (pos >= 3  && pos < 6) {
-    let iPos = 1;
-    let jPos = pos - 3;
-    return {iPos, jPos};
-  } else if (pos >= 6  && pos < 9) {
-    let iPos = 1;
-    let jPos = pos - 6;
-    return {iPos, jPos};
-  } else {
-    return null;
-  }
-}
-
-
-const gameFlow = () => {
-  if(xPlayer) {
-    gameboard.board[getPosition(pos).iPos][getPosition(pos).jPos] = "x";
+const placeAIMark = () => {
+  const board = gameboard.board;
+  if(xPlayer && player1.getType !== 'AI') {
+    board[randomSpot().iPos][randomSpot().jPos].innerHTML = "X";
     xPlayer = false;
-    displayController(pos, player1.getMark());
-  } else {
-    gameboard.board[getPosition(pos).iPos][getPosition(pos).jPos] = "o";
-    xPlayer = true;
-    displayController(pos, player2.getMark());
-  }
+  } 
 }
 
-const displayController = (pos, mark) => {
-  const cell = document.querySelectorAll('.cell');
-  for (let i = 0; i < cell.length; i++) {
-      cell[pos].innerHTML = mark;
-  }
+const randomSpot = () => {
+  const arr = emptySpots();
+  let x = arr[Math.floor(Math.random() * arr.length)];
+  return x;
 }
+
+
+
+const haveSameData = function (obj1, obj2) {
+  const obj1Length = Object.keys(obj1).length;
+  const obj2Length = Object.keys(obj2).length;
+
+  if (obj1Length === obj2Length) {
+      return Object.keys(obj1).every(
+          key => obj2.hasOwnProperty(key)
+              && obj2[key] === obj1[key]);
+  }
+  return false;
+}
+
+const randGen = () => {
+  let iPos = Math.floor(Math.random() * 3);
+  let jPos = Math.floor(Math.random() * 3);
+  return {iPos, jPos};
+}
+
 
 
 
