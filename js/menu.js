5016de5f10d3f9278b6e03aa38eb491018ff23d4
VenkaSri <venkasritharan@gmail.com>
2022-09-19 17:58:24 -0400

    if the users select one option and the inputs fields displays, it will be be hide once click on the other option

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,43 @@
-const selectMode = document.querySelector('.pvp');
+const selectPVP = document.querySelector('.pvp');
+const selectPVAI = document.querySelector('.pvai');
 const players = document.querySelectorAll('.player');
+const aplayers = document.querySelectorAll('.aplayer');
 const inputs = document.querySelectorAll('.pvp input');
+const ainputs = document.querySelectorAll('.pvai input');
 
-selectMode.addEventListener('click', (e) => {
-  players.forEach(i => i.classList.add('hide'));
-  inputs.forEach(i => i.classList.remove('hide'));
-})
+let pvpMode = true;
+let pvaiMode = true;
+
+const togglePVP = () => {
+  if (pvpMode) {
+    players.forEach(i => i.classList.add('hide'));
+    inputs.forEach(i => i.classList.remove('hide'));
+  } else {
+    players.forEach(i => i.classList.remove('hide'));
+    inputs.forEach(i => i.classList.add('hide'));
+  }
+}
+
+const togglePVAI = () => {
+  if (pvaiMode) {
+    aplayers.forEach(i => i.classList.add('hide'));
+    ainputs.forEach(i => i.classList.remove('hide'));
+  } else {
+    aplayers.forEach(i => i.classList.remove('hide'));
+    ainputs.forEach(i => i.classList.add('hide'));
+  }
+}
+
+selectPVP.addEventListener('click', () => {
+  pvpMode = true;
+  pvaiMode = false;
+  togglePVAI();
+  togglePVP();
+});
+
+selectPVAI.addEventListener('click', () => {
+  pvpMode = false;
+  pvaiMode = true;
+  togglePVP();
+  togglePVAI();
+});
