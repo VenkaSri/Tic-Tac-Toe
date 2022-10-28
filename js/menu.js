3d4527468888c581ef016dc84cc4b908a5e843a4
VenkaSri <venkasritharan@gmail.com>
2022-10-28 16:36:29 -0400

    added/edited select elements to both players for pvp

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ const players = document.querySelectorAll('.player');
 const aplayers = document.querySelectorAll('.aplayer');
 const inputs = document.querySelectorAll('.pvp input');
 const ainputs = document.querySelectorAll('.pvai input');
-const options = document.querySelector('select');
+const options = document.querySelectorAll('.pvp select');
 
 let pvpMode;
 let pvaiMode;
@@ -14,11 +14,11 @@ const togglePVP = () => {
   if (pvpMode) {
     players.forEach(i => i.classList.add('hide'));
     inputs.forEach(i => i.classList.remove('hide'));
-    options.classList.remove('hide');
+    options.forEach(i => i.classList.remove('hide'));
   } else {
     players.forEach(i => i.classList.remove('hide'));
     inputs.forEach(i => i.classList.add('hide'));
-    options.classList.add('hide');
+    options.forEach(i => i.classList.add('hide'));
   }
 }
 
@@ -26,9 +26,11 @@ const togglePVAI = () => {
   if (pvaiMode) {
     aplayers.forEach(i => i.classList.add('hide'));
     ainputs.forEach(i => i.classList.remove('hide'));
+    options.forEach(i => i.classList.remove('hide'));
   } else {
     aplayers.forEach(i => i.classList.remove('hide'));
     ainputs.forEach(i => i.classList.add('hide'));
+    options.forEach(i => i.classList.add('hide'));
   }
 }
 
